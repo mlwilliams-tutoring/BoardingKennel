933263250889f2b761301bef224e45ce0d31220f
Mikaila Williams <mikailalwilliams@gmail.com>
2020-06-17 14:43:36 -0400

    initial commit

---
+++ boardingkennel-ui/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav, Button } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './components/Home';
+import CheckIn from './components/CheckIn';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <div className="App-header">
+          <Navbar bg="dark" variant="dark" expanded="true" expand="lg" fixed="top">
+            <Navbar.Brand href="/">Boarding Kennel</Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link href="/checkin">Check-in Dog</Nav.Link>
+            </Nav>
+          </Navbar>
+          <div className="component">
+            <Route path="/" exact component={Home} />
+            <Route path="/checkin" component={CheckIn} />
+          </div>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main/java/com/tutoring/boardingkennel/BoardingKennelApplication.java
@@ -0,0 +1,13 @@
+package com.tutoring.boardingkennel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardingKennelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardingKennelApplication.class, args);
+	}
+
+}
