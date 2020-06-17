2ce3974ff4d1ec96c69293af7901cc4a529b43bb
Mikaila Williams <mikailalwilliams@gmail.com>
2020-06-17 15:25:42 -0400

    Form with post call and state change

---
+++ boardingkennel-ui/src/components/Utils.js
@@ -0,0 +1,18 @@
+export default class Utils {
+  static post(path, postData) {
+    fetch(path, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(postData),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      })
+  }
+}
