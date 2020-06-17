5373182ffb0bb14c8b4fa2a443a129f0c34ae3dd
Mikaila Williams <mikailalwilliams@gmail.com>
2020-06-17 15:24:01 -0400

    added homepage

---
+++ boardingkennel-ui/src/components/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Home extends Component{
+    render() {
+        return (
+            <div>
+                <p>
+                Welcome to our Boarding Kennel App!
+                </p>
+          </div>
+        );
+    }
+}
+export default Home;
