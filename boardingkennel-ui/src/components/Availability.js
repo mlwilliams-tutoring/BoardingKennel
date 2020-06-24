cad59bbbafc54e65191409d3f23323fa6fb866fa
Mikaila Williams <mikailalwilliams@gmail.com>
2020-06-24 08:29:16 -0400

    Added availability table

--- boardingkennel-ui/src/components/Availability.js
+++ boardingkennel-ui/src/components/Availability.js
@@ -4,9 +4,32 @@ class Availability extends Component {
     render() {
         return (
             <div>
-                <p>
-                    Welcome to the availability page!
-                </p>
+                <Table striped bordered hover variant="dark">
+                    <thead>
+                        <tr>
+                            <th>Kennel Size</th>
+                            <th>Number of Dogs Boarding</th>
+                            <th>Number of Available Kennels</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>Small Kennel</th>
+                            <td>6</td>
+                            <td>9</td>
+                        </tr>
+                        <tr>
+                            <th>Medium Kennel</th>
+                            <td>2</td>
+                            <td>8</td>
+                        </tr>
+                        <tr>
+                            <th>Large Kennel</th>
+                            <td>2</td>
+                            <td>3</td>
+                        </tr>
+                    </tbody>
+                </Table>
             </div>
         );
     }
