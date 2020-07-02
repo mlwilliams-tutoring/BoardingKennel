1241728a6182fa636364512e217b5013c60e5775
Mikaila Williams <mikailalwilliams@gmail.com>
2020-07-02 09:11:23 -0400

    Added search filter for check out page

--- boardingkennel-ui/src/components/CheckOut.js
+++ boardingkennel-ui/src/components/CheckOut.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Utils from './Utils';
 import ClickCard from './ClickCard';
+import Form from 'react-bootstrap/Form';
 
 class CheckOut extends Component {
     defaultState = {
-        boardingDogs: []
+        boardingDogs: [],
+        search: ''
     }
 
     constructor() {
@@ -28,22 +30,37 @@ class CheckOut extends Component {
             });
     }
 
+    onChange = event => {
+        this.setState({ search: event.target.value });
+    }
+
     render() {
         return (
-            <div className="grid-page">
-                {
-                    this.state.boardingDogs.map(dog => {
-                        return (<ClickCard
-                            key={'dog-card' + dog.name}
-                            target={dog}
-                            title={dog.name}
-                            text={<span><span>{'Dog Breed: ' + dog.breed}<br /></span>
-                                <span>{'Dog Age: ' + dog.age}<br /></span>
-                                <span>{'Kennel Size: ' + dog.kennelSize}</span></span>}
-                            onButtonClick={this.checkout}
-                        />);
-                    })
-                }
+            <div>
+                <div className="search-bar">
+                    <Form.Control type="text" placeholder="Search" onChange={this.onChange} />
+                </div>
+                <div className="grid-page">
+                    {
+                        this.state.boardingDogs.map(dog => {
+                            const search = this.state.search;
+
+                            if (search !== ''
+                                && dog.name.toLowerCase().indexOf(search.toLowerCase()) === -1) {
+                                return null;
+                            }
+                            return (<ClickCard
+                                key={'dog-card' + dog.name}
+                                target={dog}
+                                title={dog.name}
+                                text={<span><span>{'Dog Breed: ' + dog.breed}<br /></span>
+                                    <span>{'Dog Age: ' + dog.age}<br /></span>
+                                    <span>{'Kennel Size: ' + dog.kennelSize}</span></span>}
+                                onButtonClick={this.checkout}
+                            />);
+                        })
+                    }
+                </div>
             </div>
         );
     }
